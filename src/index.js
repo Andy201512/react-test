853daed7f286699c23ae0df24be32e35462b357a
andy201512 <1743535684@qq.com>
2022-04-27 10:18:52 +0800

    游戏改进：使用两个循环来渲染出棋盘的格子，而不是在代码里写死（hardcode）。

--- src/index.js
+++ src/index.js
@@ -21,23 +21,19 @@ class Board extends React.Component {
   }
 
   render() {
+    const rows = Array(3).fill(null).map((value, row_index) => {
+      const cols = Array(3).fill(null).map((value, col_index) => {
+        return this.renderSquare( 3 * row_index + col_index )
+      }) 
+      return (
+        <div className="board-row">
+          { cols }
+        </div>
+      )
+    });
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        { rows }
       </div>
     );
   }
